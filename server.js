2b5566a8a2ff14f54b57a9b11a542d71078a452c
Wesley Rocha <wesley.rocha@hexagonagriculture.com>
2019-08-13 17:24:21 -0300

    Changing emitted events

--- server.js
+++ server.js
@@ -10,24 +10,23 @@ var io = socket(server);
 var rooms = {};
 
 app.get('/rooms', function (req, res) {
-	res.send(JSON.stringify(rooms) + '\n');
+    res.send(JSON.stringify(rooms) + '\n');
 });
 
 server.listen(process.env.PORT || 5000, function () {
-	console.log('Starting server on port 5000.')
+    console.log('Starting server on port 5000.')
 });
 
 io.on('connection', function (socket) {
     console.log('Socket connected: ' + socket.id);
-    
+
     socket.on('join_room', function (roomname) {
         if (!rooms.hasOwnProperty(roomname)) {
             rooms[roomname] = [];
-            io.emit('room_created', roomname);
-            console.log('Room created: ' + roomname);
+            io.emit('created_room', roomname);
         }
         socket.join(roomname);
         rooms[roomname].push(socket.id);
-        console.log(socket.id + ' joined the room ' + roomname);
+        io.emit('joined_room', socket.id);
     });
 });
